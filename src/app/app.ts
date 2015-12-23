de811ed5483feac34bc699d5e90be491321d11b5
Lukas Ruebbelke <simpul@gmail.com>
2015-12-22 18:24:13 -0700

    Cleaning up files

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,24 +1,12 @@
 import {Component} from 'angular2/core';
-import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
-import {Categories} from './categories/categories';
-import {Bookmarks} from './categories/bookmarks/bookmarks';
-import {BookmarkCreate} from './categories/bookmarks/create/bookmark-create';
-import {BookmarkEdit} from './categories/bookmarks/edit/bookmark-edit';
-import {BookmarksService} from './providers/bookmarks-service';
 
 @Component({
   selector: 'app',
-  providers: [BookmarksService],
-  directives: [ ROUTER_DIRECTIVES, Categories ],
-  pipes: [],
   template: `
     <main>
         <div class="container-fluid">
             <div class="row">
-                <categories></categories>
-                <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-                    <router-outlet></router-outlet>
-                </div>
+              <h1>Hello Angular 2 Eggly!</h1>
             </div>
         </div>
     </main>
@@ -32,11 +20,5 @@ import {BookmarksService} from './providers/bookmarks-service';
       `
   ]
 })
-
-@RouteConfig([
-  { path: '/', component: Bookmarks, name: 'Bookmarks' },
-  { path: '/:category/bookmarks/create', component: BookmarkCreate, name: 'Create' },
-  { path: '/:category/bookmarks/:bookmarkId/edit', component: BookmarkEdit, name: 'Edit' }
-])
 
 export class App { }
