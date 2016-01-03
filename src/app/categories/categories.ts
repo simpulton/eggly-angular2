8a6dfab93770be2eabeffb17243c703bcff7efa0
Lukas Ruebbelke <simpul@gmail.com>
2016-01-02 21:10:00 -0700

    simplifying branch

--- src/app/categories/categories.ts
+++ src/app/categories/categories.ts
@@ -1,27 +1,16 @@
 import {Component} from 'angular2/core';
-import {ROUTER_DIRECTIVES} from 'angular2/router';
-import {Http} from 'angular2/http';
-import {CategoriesService} from '../providers/categories-service';
-import {Category} from '../providers/category-model';
 
 @Component({
   selector: 'categories',
-  providers: [ CategoriesService ],
-  pipes: [],
   template: require('./categories.tmpl.html'),
   styles: [require('./categories.css')]
 })
 
 export class Categories {
-    public categories: Category[];
-
-    constructor(public CategoriesService: CategoriesService) {};
-
-    ngOnInit() {
-        this.CategoriesService.getCategories()
-            .then(
-                (data: Category[]) => this.categories = data,
-                error => console.error(error)
-            );
-    }
+    public categories: Object[] = [
+        {'id': 0, 'name': 'Development'},
+        {'id': 1, 'name': 'Design'},
+        {'id': 2, 'name': 'Exercise'},
+        {'id': 3, 'name': 'Humor'}
+    ];
 }
--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,11 +1,9 @@
 import {bootstrap} from 'angular2/platform/browser';
 import {ELEMENT_PROBE_PROVIDERS} from 'angular2/platform/common_dom';
-import {HTTP_PROVIDERS} from 'angular2/http';
 import {App} from './app/app';
 
 export function main() {
   return bootstrap(App, [
-    HTTP_PROVIDERS,
     ELEMENT_PROBE_PROVIDERS
   ])
   .catch(err => console.error(err));
