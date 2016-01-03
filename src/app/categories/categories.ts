ecea8190b4d099985cd74dd43d090ecb8384e3d6
Lukas Ruebbelke <simpul@gmail.com>
2016-01-02 21:21:54 -0700

    cleaning up

--- src/app/categories/categories.ts
+++ src/app/categories/categories.ts
@@ -2,7 +2,6 @@ import {Component} from 'angular2/core';
 import {ROUTER_DIRECTIVES} from 'angular2/router';
 import {Http} from 'angular2/http';
 import {CategoriesService} from '../providers/categories-service';
-import {Category} from '../providers/category-model';
 
 @Component({
   selector: 'categories',
@@ -13,14 +12,14 @@ import {Category} from '../providers/category-model';
 })
 
 export class Categories {
-    public categories: Category[];
+    public categories: Object[];
 
     constructor(public CategoriesService: CategoriesService) {};
 
     ngOnInit() {
         this.CategoriesService.getCategories()
             .then(
-                (data: Category[]) => this.categories = data,
+                (data: Object[]) => this.categories = data,
                 error => console.error(error)
             );
     }
--- src/app/providers/categories-service.ts
+++ src/app/providers/categories-service.ts
@@ -1,25 +1,24 @@
 import {Injectable} from 'angular2/core';
 import {Http} from 'angular2/http';
 import {find} from 'lodash';
-import {Category} from './category-model';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class CategoriesService {
-    private categories: Category[];
+    private categories: Object[];
     private URLS: {FETCH: string} = {
         FETCH: 'data/categories.json'
     };
 
     constructor(public http: Http) {};
 
-    cacheCategories(result: Category[]): Category[] {
+    cacheCategories(result: Object[]): Object[] {
         this.categories = result;
         return this.categories;
     };
 
-    getCategories(): Promise<Category[]> {
+    getCategories(): Promise<Object[]> {
         return this.categories
             ? Promise.resolve(this.categories)
             : this.http.get(this.URLS.FETCH)
