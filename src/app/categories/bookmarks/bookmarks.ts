e15c23655da75b5292e682132f7bbf4ec0593157
Lukas Ruebbelke <simpul@gmail.com>
2016-01-03 00:09:43 -0700

    bookmark service example

--- src/app/categories/bookmarks/bookmarks.ts
+++ src/app/categories/bookmarks/bookmarks.ts
@@ -1,5 +1,7 @@
 import {Component} from 'angular2/core';
 import {RouteParams, ROUTER_DIRECTIVES} from 'angular2/router';
+import {clone} from 'lodash';
+import {BookmarksService} from '../../providers/bookmarks-service';
 import {Bookmark} from '../../providers/bookmark-model';
 import {CategoriesService} from '../../providers/categories-service';
 import {Category} from '../../providers/category-model';
@@ -7,31 +9,31 @@ import {CategoryFilter} from '../category-filter';
 
 @Component({
   selector: 'bookmarks',
-  providers: [CategoriesService],
+  providers: [BookmarksService, CategoriesService],
   directives: [ ROUTER_DIRECTIVES ],
   pipes: [CategoryFilter],
   template: require('./bookmarks.tmpl.html')
 })
 
 export class Bookmarks {
-    public bookmarks: Bookmark[] = [
-        {'id':0, 'title': 'AngularJS', 'url': 'http://angularjs.org', 'category': 'Development' },
-        {'id':1, 'title': 'Egghead.io', 'url': 'http://egghead.io', 'category': 'Development' },
-        {'id':2, 'title': 'A List Apart', 'url': 'http://alistapart.com/', 'category': 'Design' },
-        {'id':3, 'title': 'One Page Love', 'url': 'http://onepagelove.com/', 'category': 'Design' },
-        {'id':4, 'title': 'MobilityWOD', 'url': 'http://www.mobilitywod.com/', 'category': 'Exercise' },
-        {'id':5, 'title': 'Robb Wolf', 'url': 'http://robbwolf.com/', 'category': 'Exercise' },
-        {'id':6, 'title': 'Senor Gif', 'url': 'http://memebase.cheezburger.com/senorgif', 'category': 'Humor' },
-        {'id':7, 'title': 'Wimp', 'url': 'http://wimp.com', 'category': 'Humor' },
-        {'id':8, 'title': 'Dump', 'url': 'http://dump.com', 'category': 'Humor' }
-    ];
+    public bookmarks: Bookmark[];
 
     constructor(
+        public BookmarksService: BookmarksService,
         public CategoriesService: CategoriesService,
         private RouteParams: RouteParams
     ) {}
 
     ngOnInit() {
         this.CategoriesService.setCurrentCategory(this.RouteParams.get('category'));
+        this.getBookmarks();
+    }
+
+    getBookmarks(): void {
+        this.BookmarksService.getBookmarks()
+            .then(
+                data => this.bookmarks = clone(data),
+                error => console.error(error)
+            );
     }
 }
---
+++ src/app/providers/bookmarks-service.ts
@@ -0,0 +1,29 @@
+import {Injectable} from 'angular2/core';
+import {Http} from 'angular2/http';
+import {Bookmark} from './bookmark-model';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
+
+@Injectable()
+export class BookmarksService {
+    private bookmarks: Bookmark[];
+    private URLS: {FETCH: string} = {
+        FETCH: 'data/bookmarks.json'
+    };
+
+    constructor(public http: Http) {};
+
+    getBookmarks(): Promise<Bookmark[]> {
+        return this.bookmarks
+            ? Promise.resolve(this.bookmarks)
+            : this.http.get(this.URLS.FETCH)
+                .map(res => {
+                    return this.cacheBookmarks(res.json());
+                }).toPromise();
+    }
+
+    cacheBookmarks(result: Bookmark[]): Bookmark[] {
+        this.bookmarks = result;
+        return this.bookmarks;
+    }
+}
