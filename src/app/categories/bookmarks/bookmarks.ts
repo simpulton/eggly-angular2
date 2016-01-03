317f826a921a3dd64c7cc7083667b52d0b1b0c61
Lukas Ruebbelke <simpul@gmail.com>
2016-01-02 22:28:43 -0700

    bookmarks component example

--- src/app/categories/bookmarks/bookmarks.ts
+++ src/app/categories/bookmarks/bookmarks.ts
@@ -2,9 +2,19 @@ import {Component} from 'angular2/core';
 
 @Component({
   selector: 'bookmarks',
-  template: `
-    <h1>Hello bookmarks!</h1>
-  `
+  template: require('./bookmarks.tmpl.html')
 })
 
-export class Bookmarks {}
+export class Bookmarks {
+    public bookmarks: Object[] = [
+        {'id':0, 'title': 'AngularJS', 'url': 'http://angularjs.org', 'category': 'Development' },
+        {'id':1, 'title': 'Egghead.io', 'url': 'http://egghead.io', 'category': 'Development' },
+        {'id':2, 'title': 'A List Apart', 'url': 'http://alistapart.com/', 'category': 'Design' },
+        {'id':3, 'title': 'One Page Love', 'url': 'http://onepagelove.com/', 'category': 'Design' },
+        {'id':4, 'title': 'MobilityWOD', 'url': 'http://www.mobilitywod.com/', 'category': 'Exercise' },
+        {'id':5, 'title': 'Robb Wolf', 'url': 'http://robbwolf.com/', 'category': 'Exercise' },
+        {'id':6, 'title': 'Senor Gif', 'url': 'http://memebase.cheezburger.com/senorgif', 'category': 'Humor' },
+        {'id':7, 'title': 'Wimp', 'url': 'http://wimp.com', 'category': 'Humor' },
+        {'id':8, 'title': 'Dump', 'url': 'http://dump.com', 'category': 'Humor' }
+    ];
+}
