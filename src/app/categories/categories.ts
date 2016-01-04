2b231f24d8848dcb1a379fc1abf62324705cc4d2
Jonathan Garvey <jon.d.garvey@gmail.com>
2016-01-04 15:44:47 -0700

    format cleanup: for categories component

--- src/app/categories/categories.ts
+++ src/app/categories/categories.ts
@@ -3,11 +3,11 @@ import {Component} from 'angular2/core';
 @Component({
   selector: 'categories',
   template: require('./categories.tmpl.html'),
-  styles: [require('./categories.css')]
+  styles: [ require('./categories.css') ]
 })
 
 export class Categories {
-  public categories: Object[] = [
+  categories: Object[] = [
     { 'id': 0, 'name': 'Development' },
     { 'id': 1, 'name': 'Design' },
     { 'id': 2, 'name': 'Exercise' },
