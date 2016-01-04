c6c82e03d1231c61d5839682d5b1f31834f80e66
Jonathan Garvey <jon.d.garvey@gmail.com>
2016-01-04 13:56:58 -0700

    format: for category filter

--- src/app/categories/bookmarks/bookmarks.ts
+++ src/app/categories/bookmarks/bookmarks.ts
@@ -14,24 +14,24 @@ import {CategoryFilter} from '../category-filter';
 })
 
 export class Bookmarks {
-    public bookmarks: Bookmark[] = [
-        {'id':0, 'title': 'AngularJS', 'url': 'http://angularjs.org', 'category': 'Development' },
-        {'id':1, 'title': 'Egghead.io', 'url': 'http://egghead.io', 'category': 'Development' },
-        {'id':2, 'title': 'A List Apart', 'url': 'http://alistapart.com/', 'category': 'Design' },
-        {'id':3, 'title': 'One Page Love', 'url': 'http://onepagelove.com/', 'category': 'Design' },
-        {'id':4, 'title': 'MobilityWOD', 'url': 'http://www.mobilitywod.com/', 'category': 'Exercise' },
-        {'id':5, 'title': 'Robb Wolf', 'url': 'http://robbwolf.com/', 'category': 'Exercise' },
-        {'id':6, 'title': 'Senor Gif', 'url': 'http://memebase.cheezburger.com/senorgif', 'category': 'Humor' },
-        {'id':7, 'title': 'Wimp', 'url': 'http://wimp.com', 'category': 'Humor' },
-        {'id':8, 'title': 'Dump', 'url': 'http://dump.com', 'category': 'Humor' }
-    ];
+  public bookmarks: Bookmark[] = [
+    { 'id': 0, 'title': 'AngularJS', 'url': 'http://angularjs.org', 'category': 'Development' },
+    { 'id': 1, 'title': 'Egghead.io', 'url': 'http://egghead.io', 'category': 'Development' },
+    { 'id': 2, 'title': 'A List Apart', 'url': 'http://alistapart.com/', 'category': 'Design' },
+    { 'id': 3, 'title': 'One Page Love', 'url': 'http://onepagelove.com/', 'category': 'Design' },
+    { 'id': 4, 'title': 'MobilityWOD', 'url': 'http://www.mobilitywod.com/', 'category': 'Exercise' },
+    { 'id': 5, 'title': 'Robb Wolf', 'url': 'http://robbwolf.com/', 'category': 'Exercise' },
+    { 'id': 6, 'title': 'Senor Gif', 'url': 'http://memebase.cheezburger.com/senorgif', 'category': 'Humor' },
+    { 'id': 7, 'title': 'Wimp', 'url': 'http://wimp.com', 'category': 'Humor' },
+    { 'id': 8, 'title': 'Dump', 'url': 'http://dump.com', 'category': 'Humor' }
+  ];
 
-    constructor(
-        public CategoriesService: CategoriesService,
-        private RouteParams: RouteParams
-    ) {}
+  constructor(
+    public CategoriesService: CategoriesService,
+    private RouteParams: RouteParams
+  ) { }
 
-    ngOnInit() {
-        this.CategoriesService.setCurrentCategory(this.RouteParams.get('category'));
-    }
+  ngOnInit() {
+    this.CategoriesService.setCurrentCategory(this.RouteParams.get('category'));
+  }
 }
