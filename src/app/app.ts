33ad2bd931a21d612c2f0156d7f9e0d717ab8a88
Jonathan Garvey <jon.d.garvey@gmail.com>
2016-01-04 13:56:58 -0700

    format: app for compiling

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,20 +4,20 @@ import {Component} from 'angular2/core';
   selector: 'app',
   template: `
     <main>
-        <div class="container-fluid">
-            <div class="row">
-              <h1>Hello Angular 2 Eggly!</h1>
-            </div>
+      <div class="container-fluid">
+        <div class="row">
+          <h1>Hello Angular 2 Eggly!</h1>
         </div>
+      </div>
     </main>
   `,
   styles: [
-      `
-          .main {
-            padding-top: 96px;
-            padding-left: 24px;
-          }
-      `
+    `
+      .main {
+        padding-top: 96px;
+        padding-left: 24px;
+      }
+    `
   ]
 })
 
