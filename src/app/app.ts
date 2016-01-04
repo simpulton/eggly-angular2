b0879fffece8bc3831c207a64e36823c617d565b
Jonathan Garvey <jon.d.garvey@gmail.com>
2016-01-04 13:56:58 -0700

    format: for component router

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,22 +8,24 @@ import {Bookmarks} from './categories/bookmarks/bookmarks';
   directives: [ ROUTER_DIRECTIVES, Categories ],
   template: `
     <main>
-        <div class="container-fluid">
-            <div class="row">
-                <categories></categories>
-                <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-                    <router-outlet></router-outlet>
-                </div>
-            </div>
+      <div class="container-fluid">
+        <div class="row">
+          <categories></categories>
+          <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+            <router-outlet></router-outlet>
+          </div>
         </div>
+      </div>
     </main>
   `,
-  styles: [`
+  styles: [
+    `
       .main {
         padding-top: 96px;
         padding-left: 24px;
       }
-  `]
+    `
+  ]
 })
 
 @RouteConfig([
